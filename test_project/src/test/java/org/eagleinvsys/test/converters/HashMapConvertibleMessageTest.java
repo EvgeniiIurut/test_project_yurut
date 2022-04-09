39cc9f524be110bf2910e4ac487b967de6d48dc6
Evgenii <john.yurut@gmail.com>
2022-04-09 16:27:36 +0300

    Third commit

--- test_project/src/test/java/org/eagleinvsys/test/converters/HashMapConvertibleMessageTest.java
+++ test_project/src/test/java/org/eagleinvsys/test/converters/HashMapConvertibleMessageTest.java
@@ -1,6 +1,12 @@
 package org.eagleinvsys.test.converters;
 
+import org.eagleinvsys.test.converters.impl.ConvertibleCollectionImpl;
+import org.eagleinvsys.test.converters.impl.HashMapConvertibleMessage;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,5 +14,15 @@ class HashMapConvertibleMessageTest {
 
     @Test
     void getElement() {
+        // given
+        Map<String, String> mapToCheck = Map.of("Name", "Bob",
+                "Last name", "Dylan",
+                "Age", "56");
+        final ConvertibleMessage hashMapConvertibleMessage = new HashMapConvertibleMessage(mapToCheck);
+        // when
+        String expectedResult = mapToCheck.get("Age");
+        String ageOfElement = hashMapConvertibleMessage.getElement("Age");
+        // then
+        assertEquals(ageOfElement, expectedResult);
     }
 }
